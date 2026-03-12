9c4631cf808ed99ded3fd4262fe180ce02b5ab7f
garg-tejas <tejas.garg.130805@gmail.com>
2026-03-12 09:07:23 +0530

    feat: add graph command enum and supervisor wiring with thin session adapters

--- backend/graph/commands.py
+++ backend/graph/commands.py
@@ -8,4 +8,8 @@ class GraphCommand(StrEnum):
     CONFIRM_TOPIC = "confirm_topic"
     NUDGE_DISCOVERY = "nudge_discovery"
     UPDATE_APPROVED_PAPERS = "update_approved_papers"
+    START_ANALYSIS = "start_analysis"
+    START_SURVEY = "start_survey"
+    REVISE_SURVEY = "revise_survey"
+    APPROVE_SURVEY = "approve_survey"
 
